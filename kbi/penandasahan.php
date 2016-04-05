0db29f98ded72d43a5ddc0d7791b134ba2ba5d9f
hariesa <hariesa.prabowo@gmail.com>
2016-04-05 15:05:48 +0700

    Added files via upload

---
+++ kbi/penandasahan.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<table width="85%" border="1" cellspacing="2" cellpadding="2">
+  	<tr>
+    	<Td>Status PenandasahanStatus Penandasahan</Td>
+    	<Td width="63%">&nbsp;</Td>
+    </tr>
+  	<tr>
+  	  <Td><form id="form2" name="form2" method="post" action="">
+  	    <input type="radio" name="radio" id="radio" value="radio" />
+  	    <label for="radio"></label>
+  	    Penandasahan
+<label for="checkbox"></label>
+      <input type="radio" name="radio2" id="radio2" value="radio2" />
+      <label for="radio2"></label>
+  	  Dikembalikan 
+  	  <input type="radio" name="radio3" id="radio3" value="radio3" />
+  	  <label for="radio3"></label>
+  	  <label for="checkbox"></label>
+  	  Di tolak
+  	  </form></Td>
+  	  <Td>&nbsp;</Td>
+  </tr>
+  	<tr>
+  	  <Td>Nomor RIB </Td>
+  	  <Td><label for="textfield22"></label>
+      <input type="text" name="textfield22" id="textfield22" /> 
+      (system)</Td>
+  </tr>
+  	<tr>
+  	  <Td>Tanggal RIB</Td>
+  	  <Td><input type="text" name="textfield23" id="textfield23" /> 
+  	    (system)</Td>
+  </tr>
+  	<tr>
+  	  <Td>Nomor Surat Penandasahan</Td>
+  	  <Td><input type="text" name="textfield24" id="textfield24" /> 
+  	    (system)</Td>
+  </tr>
+  	<tr>
+  	  <Td>Tanggal Surat Penandasahan</Td>
+  	  <Td><input type="text" name="textfield25" id="textfield25" /></Td>
+  </tr>
+  	<tr>
+  	  <Td>Dokumen Penandasahan</Td>
+  	  <Td><label for="fileField"></label>
+      <input type="file" name="fileField" id="fileField" /></Td>
+  </tr>
+  	<tr>
+  	  <Td colspan="2"><input type="submit" name="button" id="button" value="Simpan" /></Td>
+  </tr>
+</table>
+
+
+
+<table width="85%" border="1" cellspacing="2" cellpadding="2">
+  <tr>
+    <td width="20%">Nomor RKBI/Masterlist</td>
+    <td width="3%">:</td>
+    <td width="26%"><label for="textfield"></label>
+      <input type="text" name="textfield" id="textfield" /></td>
+    <td width="17%">Sub Kontraktor</td>
+    <td width="6%">:</td>
+    <td width="28%"><input type="text" name="textfield2" id="textfield2" /></td>
+  </tr>
+  <tr>
+    <td>Tanggal RKBI/Masterlist</td>
+    <td>:</td>
+    <td><input type="text" name="textfield5" id="textfield5" /></td>
+    <td>No Kontrak / tanggal</td>
+    <td>:</td>
+    <td><input type="text" name="textfield3" id="textfield3" /></td>
+  </tr>
+  <tr>
+    <td valign="top">NPWP</td>
+    <td valign="top">:</td>
+    <td valign="top"><input type="text" name="textfield6" id="textfield6" />
+      (By Login)</td>
+    <td valign="top">Status Barang</td>
+    <td valign="top">:</td>
+    <td valign="top"><input name="textfield4" type="text" id="textfield4" value="Bukan Sewa" />
+      (mengikuti form pendaftaran yang dipilih</td>
+  </tr>
+  <tr>
+    <td>Kontraktor KPS/KKS</td>
+    <td>:</td>
+    <td><input type="text" name="textfield7" id="textfield7" />
+      (By Login)</td>
+    <td>KPBC Pemasukan</td>
+    <td>:</td>
+    <td><input name="textfield13" type="text" id="textfield13" value="Bukan Sewa" /></td>
+  </tr>
+  <tr>
+    <td>Alamat</td>
+    <td>:</td>
+    <td><input type="text" name="textfield8" id="textfield8" />
+      (By Login)</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Dasar Kontrak KPS/KKS</td>
+    <td>:</td>
+    <td><input type="text" name="textfield9" id="textfield9" /></td>
+    <td colspan="3">Kontak Korespondensi</td>
+  </tr>
+  <tr>
+    <td>Tanggal Kontrak</td>
+    <td>:</td>
+    <td><input type="text" name="textfield10" id="textfield10" /></td>
+    <td>Nama</td>
+    <td>:</td>
+    <td><input type="text" name="textfield14" id="textfield14" /></td>
+  </tr>
+  <tr>
+    <td>Masa Berlaku</td>
+    <td>:</td>
+    <td><input type="text" name="textfield11" id="textfield11" /></td>
+    <td>Email/HP</td>
+    <td>:</td>
+    <td><input type="text" name="textfield15" id="textfield15" /></td>
+  </tr>
+  <tr>
+    <td>Daerah Operasi</td>
+    <td>:</td>
+    <td><input type="text" name="textfield12" id="textfield12" /></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<p>
+                        
+       
+<form id="form1" name="form1" method="post" action="">
+   
+  <table width="88%" border="1" cellspacing="2" cellpadding="2">
+   
+    <tr>
+      <td width="10%">KIMAP</td>
+      <td width="10%">Sub Kimap</td>
+      <td width="21%">Uraian</td>
+      <td width="10%">Kondisi</td>
+      <td width="10%">Pos Tarif</td>
+      <td width="7%">Jumlah</td>
+      <td width="7%">Satuan</td>
+      <td width="12%">Perkiraan Nilai</td>
+      <td width="10%">Keterangan</td>
+    </tr>
+    <tr>
+      <td><label for="select"></label>
+      <input name="textfield16" type="text" id="textfield16" size="8" /></td>
+      <td><input name="textfield18" type="text" id="textfield18" size="8" /></td>
+      <td><label for="textfield"></label>
+      <input name="textfield" type="text" id="textfield" size="8" /></td>
+      <td><label for="select2"></label>
+      <input name="textfield20" type="text" id="textfield20" size="8" /></td>
+      <td><label for="textfield2"></label>
+      <input name="textfield2" type="text" id="textfield2" size="8" /></td>
+      <td><label for="textfield3"></label>
+      <input name="textfield3" type="text" id="textfield3" size="8" /></td>
+      <td><label for="textfield4"></label>
+      <input name="textfield4" type="text" id="textfield4" size="8" /></td>
+      <td><label for="textfield5"></label>
+      <input name="textfield5" type="text" id="textfield5" size="8" /></td>
+      <td><label for="textfield8"></label>
+      <input type="text" name="textfield8" id="textfield8" /></td>
+    </tr>
+   <tr>
+      <td><label for="select"></label>
+      <input name="textfield17" type="text" id="textfield17" size="8" /></td>
+      <td><input name="textfield19" type="text" id="textfield19" size="8" /></td>
+      <td><label for="textfield"></label>
+      <input name="textfield" type="text" id="textfield" size="8" /></td>
+      <td><label for="select2"></label>
+      <input name="textfield21" type="text" id="textfield21" size="8" /></td>
+      <td><label for="textfield2"></label>
+      <input name="textfield2" type="text" id="textfield2" size="8" /></td>
+      <td><label for="textfield3"></label>
+      <input name="textfield3" type="text" id="textfield3" size="8" /></td>
+      <td><label for="textfield4"></label>
+      <input name="textfield4" type="text" id="textfield4" size="8" /></td>
+      <td><label for="textfield5"></label>
+      <input name="textfield5" type="text" id="textfield5" size="8" /></td>
+      <td><label for="textfield8"></label>
+      <input type="text" name="textfield8" id="textfield8" /></td>
+    </tr>
+  </table>
+  <p>
+    <input type="submit" name="button2" id="button2" value="Save" />
+
+  </p>
+</form>
+
+
+
+</body>
+</html>
