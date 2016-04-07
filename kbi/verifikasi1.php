418a6650c6d9482bb3c3628fa584dd2c8fb4094d
ahmadchoi <ahmadchoirul55@gmail.com>
2016-04-07 11:29:42 +0700

    Update verifikasi1.php

--- kbi/verifikasi1.php
+++ kbi/verifikasi1.php
@@ -63,7 +63,7 @@
 	        display: none;
 	        text-align: left;
 	        width: 100%;
-	        font-size: 14px;
+	        font-size: 11px;
 	        line-height: 140%;
 	        padding-top: 10px;
 	        background: white;
@@ -118,14 +118,14 @@
     <td colspan="5">Status Verifikasi</td>
   </tr>
   <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td><input name="" type="radio" value="">Di verifikasi</td>
+    <td><input name="" type="radio" value="">Di tolak</td>
+    <td><input name="" type="radio" value="">Di terima</td>
     <td>&nbsp;</td>
     <td>&nbsp;</td>
   </tr>
   <tr>
-    <td><input type="submit" name="button" id="button" value="Simpan" /></td>
+    <td><input type="submit" name="button" class="button" value="Simpan" /></td>
     <td>&nbsp;</td>
     <td>&nbsp;</td>
     <td>&nbsp;</td>
@@ -340,74 +340,56 @@
     <tr>
       <td>A1</td>
       <td>Surat Pengajuan RKBI Sudah Benar, disertai dokumen pendukungnya</td>
-      <td><input type="radio" name="radio" id="radio3" value="radio" />
-      <label for="radio3"></label></td>
-      <td><input type="radio" name="radio" id="radio4" value="radio" />
-      <label for="radio4"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>A2</td>
       <td>No &amp; Tgl RKBI/ML telah dicantumkan, distempel dan tandatangan asli</td>
-      <td><input type="radio" name="radio" id="radio5" value="radio" />
-      <label for="radio5"></label></td>
-      <td><input type="radio" name="radio" id="radio6" value="radio" />
-      <label for="radio6"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>A3</td>
       <td>Urutan No Halaman sudah benar</td>
-      <td><input type="radio" name="radio" id="radio7" value="radio" />
-      <label for="radio7"></label></td>
-      <td><input type="radio" name="radio" id="radio8" value="radio" />
-      <label for="radio8"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>A4</td>
       <td>Setiap lajur sudah disusun sesuai dengan KIMAP dan deskrpsinya</td>
-      <td><input type="radio" name="radio" id="radio9" value="radio" />
-      <label for="radio9"></label></td>
-      <td><input type="radio" name="radio" id="radio10" value="radio" />
-      <label for="radio10"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>A5</td>
       <td>Jumlah satuan telah dibuat untuk setiap lajur</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-      <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-      <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>A6</td>
       <td>Perkiraan Nilai dibuat untuk tiap lajur</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>A7</td>
       <td>Lokasi penggunaan dibuat untuk setiap lajur</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>A8</td>
       <td>Tujuan penggunaan dibuat untuk setiap lajur</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>A9</td>
       <td>Kolom Keterangan diisi asset capital, goods consumable good atau impor sementara</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>II</td>
@@ -418,28 +400,22 @@
     <tr>
       <td>B1</td>
       <td>Perusahaan, Wilayah Kerja dan sub kontraktor (bila ada) ditulis dengan benar</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>B2</td>
       <td>No Kontrak dan berakhirnya kontrak (bila ada) berlaku</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>B3</td>
       <td><p>Berhak Mendapatkan fasilitas pembebasan (BM &amp; PDRI)</p>
       <p>- PMK 20 tahun 2005</p>
       <p>- PMK 27 tahun 2012</p></td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>III</td>
@@ -450,66 +426,50 @@
     <tr>
       <td>1</td>
       <td>Status penggolongan barang sudah selesai</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>2</td>
       <td>Pos tarif (HS Code) sudah ada dalam RKBI</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>3</td>
       <td>Kode KIMAP dan deskripsi telah sesuai</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>4</td>
       <td>Jenis barang standard dan spesifikasi</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>5</td>
       <td>Pengajuan barang sesuai dengan dengan tujuan penggunaan</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>6</td>
       <td>Jumlah kebutuhan barang sudah sesuai</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>7</td>
       <td>Jumlah Nilai total telah benar</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>8</td>
       <td>Kolom keterangan telah diisi dengan lengkap</td>
-      <td><input type="radio" name="radio" id="radio11" value="radio" />
-        <label for="radio11"></label></td>
-      <td><input type="radio" name="radio" id="radio12" value="radio" />
-        <label for="radio12"></label></td>
+      <td><input name="" type="checkbox" value=""></td>
+      <td><input name="" type="checkbox" value=""></td>
     </tr>
     <tr>
       <td>IV</td>
